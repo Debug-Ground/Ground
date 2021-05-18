57fd9402a58b1eae9d2aed0b4277d98a7433585d
leekyuhwan1234 <lgh4712@gmail.com>
2021-05-18 17:45:06 +0900

    Create : question detail, insert, main

--- controller/questionController.js
+++ controller/questionController.js
@@ -1,31 +1,39 @@
 var express = require('express');
-var index = require('../model/indexDao');
+var questionDao = require('../model/questionDao');
 var jwtmiddle = require('../middleware/jwt');
+var dayjs =  require('dayjs');
 
 function question(req, res, next) {
+  questionDao.getQuestion().then((db_data) => {
   let token = req.cookies.user;
   jwtmiddle.jwtCerti(token).then(
       (permission)=>{
-        res.render('question', {permission});
-      }
-  ).catch(err=>res.send("<script>alert('jwt err');</script>")); 
+        res.render('question', {db_data, q_num : req.params.num , max_value : 10, dayjs, permission});
+      }).catch(err=>res.send("<script>alert('jwt err');</script>")); 
+  }).catch(err=>res.send("<script>alert('err');</script>"));
 }
+
 function qwrite(req, res, next) {
   let token = req.cookies.user;
   jwtmiddle.jwtCerti(token).then(
       (permission)=>{
-        res.render('qwrite', {permission});
+        res.render('question_write', {permission});
       }
   ).catch(err=>res.send("<script>alert('jwt err');</script>")); 
 }
 
 function qdetail(req, res, next) {
-  let token = req.cookies.user;
-  jwtmiddle.jwtCerti(token).then(
+  var parameters = {
+    "qid": req.params.num
+  }
+  questionDao.getQuestionDetail(parameters).then((db_data) => {
+    let token = req.cookies.user;
+    jwtmiddle.jwtCerti(token).then(
       (permission)=>{
-        res.render('qdetail', {permission});
+        res.render('question_detail', {permission});
       }
-  ).catch(err=>res.send("<script>alert('jwt err');</script>")); 
+    ).catch(err=>res.send("<script>alert('jwt err');</script>")); 
+  }).catch(err=>res.send("<script>alert('err');</script>"));
 }
 
 
--- routes/question.js
+++ routes/question.js
@@ -3,8 +3,9 @@ var router = express.Router();
 var questionController = require('../controller/questionController')
 
 /* GET home page. */
-router.get('/', questionController.question);
 router.get('/write', questionController.qwrite);
+router.get('/:num', questionController.question);
+router.get('/detail/:num', questionController.qdetail)
 
 
 module.exports = router;
