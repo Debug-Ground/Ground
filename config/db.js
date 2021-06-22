9c4e5e7db5367267675d3b2a4eb233ef78771448
leekyuhwan1234 <lgh4712@gmail.com>
2021-06-22 15:23:07 +0900

    db.js env crypto

--- config/db.js
+++ config/db.js
@@ -1,13 +1,27 @@
- const { TooManyRequests } = require('http-errors')
  const mysql = require('mysql')
  require('dotenv').config();
+ const crypto = require('crypto');
+ 
+ var envArr = [process.env.host, process.env.port, process.env.user, process.env.password, process.env.database]
+ const algorithm = 'aes-192-cbc';
+ const key = process.env.SecretKey;
+ const iv = Buffer.alloc(16, 0);
+ var decipher = crypto.createDecipheriv(algorithm, key, iv);
+ var envList =[];
 
+ for(env of envArr) {
+    decipher = crypto.createDecipheriv(algorithm, key, iv);
+    let envData = decipher.update(env, 'hex', 'utf8');
+    envData += decipher.final('utf8');
+    envList.push(envData);
+    envData = "";
+ }
  const db_config = {
-     host : process.env.DB_host,
-     port : process.env.DB_port,
-     user : process.env.DB_user,
-     password : process.env.DB_password,
-     database : process.env.DB_database
+     host : envList[0],
+     port : envList[1],
+     user : envList[2],
+     password : envList[3],
+     database : envList[4]
  };
 
  const db = mysql.createConnection(db_config);
